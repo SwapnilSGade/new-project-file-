674518adeccf6d87ebb0d0755fa237694707a4e6
SwapnilSGade <swapnilgade100@gmail.com>
2026-01-05 15:46:16 +0530

    variables.tf

--- azure/variables.tf
+++ azure/variables.tf
@@ -1,23 +1,23 @@
 variable "resource_group_name" {
   description = "Name of the Azure Resource Group"
   type        = string
-  default     = "rg-crosscloud-backup"
+  default     = "rg-crosscloud-backup-pune"
 }
 
 variable "location" {
   description = "Azure region for resources"
   type        = string
-  default     = "East US"
+  default     = "Central India"
 }
 
 variable "storage_account_name" {
   description = "Name of the Azure Storage Account"
   type        = string
-  default     = "crosscloudstorageacct"
+  default     = "swapnilbackupacct"
 }
 
 variable "container_name" {
   description = "Name of the Blob container"
   type        = string
-  default     = "s3backup"
+  default     = "awsbackup"
 }
