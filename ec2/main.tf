387c54af0e2746a09da7a1a0e8d65b59b8ea418a
SwapnilSGade <swapnilgade100@gmail.com>
2026-01-05 16:09:02 +0530

    swa

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "backup_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04
+  ami           = "ami-02b8269d5e85954ef" # Ubuntu 20.04
   instance_type = "t3.micro"
   iam_instance_profile = aws_iam_instance_profile.s3_read_profile.name
 
