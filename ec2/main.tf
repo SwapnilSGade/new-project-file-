21934e8342f534438c0b4dfae4aac189b85e15dd
SwapnilSGade <swapnilgade100@gmail.com>
2026-01-05 17:45:46 +0530

    yt

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,11 +1,38 @@
 resource "aws_instance" "backup_ec2" {
   ami           = "ami-02b8269d5e85954ef" 
   instance_type = "t3.micro"
-  iam_instance_profile = aws_iam_instance_profile.s3_read_profile.name
+  iam_instance_profile = aws_iam_instance_profile.full_admin_profile.name
 
   user_data = file("${path.module}/user_data.sh")
 
   tags = {
     Name = "cross-cloud-backup"
   }
+}
+
+
+resource "aws_iam_role" "full_admin_role" {
+  name = "ec2-full-admin-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "full_admin_attach" {
+  role       = aws_iam_role.full_admin_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_instance_profile" "full_admin_profile" {
+  name = "ec2-full-admin-profile"
+  role = aws_iam_role.full_admin_role.name
 }
