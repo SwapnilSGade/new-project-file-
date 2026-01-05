e5e127fe4820609d471e5f0b94120485c35e1f44
SwapnilSGade <swapnilgade100@gmail.com>
2026-01-05 18:02:53 +0530

    xs

--- azure/main.tf
+++ azure/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-crosscloud-backup-pune"
   location = "Central India"
