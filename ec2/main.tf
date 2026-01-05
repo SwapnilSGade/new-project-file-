03a40c4f807dec79dfed3145819380c68c3476f2
SwapnilSGade <swapnilgade100@gmail.com>
2026-01-05 16:48:15 +0530

    s

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "backup_ec2" {
-  ami           = "ami-02b8269d5e85954ef" # Ubuntu 20.04
+  ami           = "ami-02b8269d5e85954ef" 
   instance_type = "t3.micro"
-  iam_instance_profile = aws_iam_instance_profile.s3_read_profile.name
+  iam_instance_profile = aws_iam_instance_profile s3_read_profile.name
 
   user_data = file("${path.module}/user_data.sh")
 
